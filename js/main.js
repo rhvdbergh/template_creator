85cd0f14731c79e4cc3379dd876c016d5161257c
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-08-11 23:57:17 -0700

    file created based on checked boxes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-console.log('connected');
-
 document.getElementById('generate_text_btn').addEventListener('click', function () {
+    let content = "";
+
+    if (document.getElementById('exampleCheck1').checked) {
+        content += "List item 1 checked\n";
+    }
+
+    if (document.getElementById('exampleCheck2').checked) {
+        content += "List item 2 checked\n";
+    }
+
+    if (document.getElementById('exampleCheck3').checked) {
+        content += "List item 3 checked\n";
+    }
+    
     downloadAsFile({
-        data: 'this is some text',
+        data: content,
         filename: 'demo.txt'
     })
 })
